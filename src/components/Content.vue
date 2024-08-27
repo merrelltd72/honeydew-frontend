41be20bdef60a98a6b4e35eb5f57d048fd0ba4fe
merrelltd72 <tyron.merrell@gmail.com>
2024-08-27 15:58:50 -0500

    Added create action.

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,10 +1,12 @@
 <script>
 import axios from "axios";
 import HoneydewsIndex from "../HoneydewsIndex.vue";
+import HoneydewNew from "../HoneydewNew.vue";
 
 export default {
   components: {
     HoneydewsIndex,
+    HoneydewNew,
   },
   data: function () {
     return {
@@ -21,12 +23,24 @@ export default {
         this.honeydews = response.data;
       });
     },
+    handleCreateHoneydew: function (params) {
+      axios
+        .post("http://localhost:5000/honeydews.json", params)
+        .then((response) => {
+          console.log("Honeydews create", response);
+          this.honeydews.push(response.data);
+        })
+        .catch((error) => {
+          console.log("photos create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <main>
+    <HoneydewNew v-on:createHoneydew="handleCreateHoneydew" />
     <HoneydewsIndex v-bind:honeydews="honeydews" />
   </main>
 </template>
